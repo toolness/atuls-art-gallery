1364a94c8c210ff86245532ea5ef51013f4d4aa6
Atul Varma <varmaa@gmail.com>
2024-05-22 20:35:27 -0400

    Make gallery actually infinite.

--- infinite_gallery.gd
+++ infinite_gallery.gd
@@ -1,19 +1,55 @@
 extends Node3D
 
 
-@onready var gallery_chunk = preload("res://moma.tscn")
+@onready var gallery_chunk_scene = preload("res://moma.tscn")
+
+@onready var player: Node3D = $Player
+
+@onready var gallery_chunks: Array[Node3D] = []
 
 var GALLERY_CHUNK_WIDTH = 28
 
+
+func get_gallery_id(x: float) -> int:
+	return floori(x / float(GALLERY_CHUNK_WIDTH))
+
+
+func sync_galleries():
+	var middle_gallery_id = get_gallery_id(player.position.x)
+	var min_gallery_id = middle_gallery_id - 1
+	var max_gallery_id = middle_gallery_id + 1
+
+	# Get rid of galleries that are far from the player.
+	var new_gallery_chunks: Array[Node3D] = []
+	for gallery_chunk in gallery_chunks:
+		var gallery_id = get_gallery_id(gallery_chunk.position.x)
+		if gallery_id < min_gallery_id or gallery_id > max_gallery_id:
+			print("Despawning gallery with id ", gallery_id, " at ", gallery_chunk.position.x)
+			remove_child(gallery_chunk)
+		else:
+			new_gallery_chunks.push_front(gallery_chunk)
+	gallery_chunks = new_gallery_chunks
+
+	# Spawn galleries that are near the player.
+	for gallery_id in [min_gallery_id, middle_gallery_id, max_gallery_id]:
+		var found = false
+		for gallery_chunk in gallery_chunks:
+			if get_gallery_id(gallery_chunk.position.x) == gallery_id:
+				found = true
+				break
+		if not found:
+			var instance: Node3D = gallery_chunk_scene.instantiate()
+			instance.position.x = gallery_id * GALLERY_CHUNK_WIDTH
+			print("Spawning new gallery with id ", gallery_id, " at ", instance.position.x)
+			add_child(instance)
+			gallery_chunks.push_front(instance)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(3):
-		var instance: Node = gallery_chunk.instantiate()
-		instance.position.x = i * GALLERY_CHUNK_WIDTH
-		add_child(instance)
-
+	sync_galleries()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	sync_galleries()
