502278c61dd76615a9cf4cc2318bbf99a8553309
Atul Varma <varmaa@gmail.com>
2024-05-26 16:01:53 -0400

    Mount paintings.

--- moma.gd
+++ moma.gd
@@ -4,6 +4,8 @@ extends Node3D
 
 @onready var gallery: Node3D = $gallery
 
+@export var painting_scene: PackedScene
+
 const MIN_WALL_MOUNT_SIZE = 2
 
 var gallery_id: int
@@ -27,20 +29,31 @@ func _ready() -> void:
 			var second: Vector3 = faces[2] - faces[0]
 			var normal = second.cross(first).normalized()
 			var width: float
+			var y_rotation: float
 			if aabb.size.x > MIN_WALL_MOUNT_SIZE:
 				# We can mount art along the x-axis.
 				width = aabb.size.x
-			elif aabb.size.y > MIN_WALL_MOUNT_SIZE:
-				# We can mount art along the y-axis.
-				width = aabb.size.y
+				if normal.z < 0:
+					y_rotation = PI
+			elif aabb.size.z > MIN_WALL_MOUNT_SIZE:
+				# We can mount art along the z-axis.
+				width = aabb.size.z
+				y_rotation = PI / 2
+				if normal.x < 0:
+					y_rotation += PI
 			else:
 				# This isn't a big enough wall to mount anything on.
 				continue
 			# TODO: Load images dynamically with:
 			# https://docs.godotengine.org/en/stable/tutorials/io/runtime_file_loading_and_saving.html
-			# TODO: Generate surfaces dynamically with:
-			# https://docs.godotengine.org/en/stable/tutorials/3d/procedural_geometry/index.html
-			print("COOL ", child.name, " ", normal, " ", width)
+			var painting: Painting = painting_scene.instantiate()
+			add_child(painting)
+			var painting_mount_point: Vector3 = mesh_instance.position + aabb.get_center()
+			painting.translate(painting_mount_point)
+			painting.rotate_y(y_rotation)
+			# TODO: Use this width to spawn multiple paintings per wall.
+			var _unused_width = width
+			# print("COOL ", child.name, " ", normal, " ", width, " ", painting_mount_point)
 
 
 func init(new_gallery_id: int) -> void:
---
+++ painting.gd
@@ -0,0 +1,9 @@
+class_name Painting
+
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# print("Painting ready.")
+	pass # Replace with function body.
