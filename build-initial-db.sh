ad27d4bb59796e22c5e23a69da788d0a1e5525a7
Atul Varma <varmaa@gmail.com>
2024-06-21 19:57:10 -0400

    Add build-initial-db.sh.

---
+++ build-initial-db.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env sh
+
+set -e
+
+export DB_NAME=initial-db.sqlite
+
+rm -f ${DB_NAME}
+
+cd rust
+cargo run --release -- --db-path=../${DB_NAME} csv
+cargo run --release -- --db-path=../${DB_NAME} layout
