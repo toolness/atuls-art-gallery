3fcfaf8bb7b13472b20eaa052ad2941f894076be
Atul Varma <varmaa@gmail.com>
2024-06-26 12:10:29 -0400

    Add player_input.gd.

---
+++ Controller/player_input.gd
@@ -0,0 +1,15 @@
+extends MultiplayerSynchronizer
+
+class_name PlayerInput
+
+@export var input_direction: Vector2
+
+func _ready():
+    pass
+
+func is_authority() -> bool:
+    return get_multiplayer_authority() == multiplayer.get_unique_id()
+
+func _process(_delta: float) -> void:
+    if is_authority():
+        input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
