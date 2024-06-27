3832aabdbf01efe393a4170f07f4bda4cf246a00
Atul Varma <varmaa@gmail.com>
2024-06-26 21:46:34 -0400

    Get rid of spawned_example scene.

--- Levels/infinite_gallery.gd
+++ Levels/infinite_gallery.gd
@@ -13,8 +13,6 @@ extends Node3D
 
 @onready var gallery_chunks: Array[Moma] = []
 
-@export var spawned_example_scene: PackedScene
-
 const GALLERY_CHUNK_WIDTH = 28
 
 const GALLERY_SPAWN_RADIUS = 1
@@ -88,22 +86,11 @@ func _ready() -> void:
 		offline_mode_player.free()
 
 	if Lobby.IS_SERVER:
-		_spawn_multiplayer_example_objects()
 		multiplayer.peer_connected.connect(_on_peer_connected)
 		multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 		# TODO: If we're not headless, spawn our own player.
 
 	sync_galleries()
-
-
-func _spawn_multiplayer_example_objects():
-	# These just exist to try out Godot's MultiplayerSpawner and MultiplayerSynchronizer classes.
-	var rng := RandomNumberGenerator.new()
-	for i in range(10):
-		var example: Node3D = spawned_example_scene.instantiate()
-		var example_position := Vector3(rng.randf_range(28.0, 38.0), 1.0, rng.randf_range(1.0, 7.0))
-		example.translate(example_position)
-		add_child(example, true)
 
 
 func _process(_delta) -> void:
