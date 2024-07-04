f4932ef890ff8708a3128158b04fe10ef8b615d8
Atul Varma <varmaa@gmail.com>
2024-07-03 20:42:30 -0400

    Add support for loading small image.

--- rust/gallery/src/met_api.rs
+++ rust/gallery/src/met_api.rs
@@ -1,6 +1,23 @@
+use std::fmt::Display;
+
 use crate::gallery_cache::GalleryCache;
 use anyhow::{anyhow, Result};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+pub enum ImageSize {
+    Small,
+    Large,
+}
+
+impl Display for ImageSize {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ImageSize::Small => write!(f, "small"),
+            ImageSize::Large => write!(f, "large"),
+        }
+    }
+}
 
 pub fn load_met_api_record(cache: &GalleryCache, object_id: u64) -> Result<MetObjectApiRecord> {
     let filename = format!("object-{}.json", object_id);
@@ -23,6 +40,9 @@ pub struct MetObjectApiRecord {
 
     #[serde(rename = "primaryImageSmall")]
     pub primary_image_small: String,
+
+    #[serde(rename = "primaryImage")]
+    pub primary_image: String,
 
     #[serde(rename = "objectDate")]
     pub object_date: String,
@@ -54,21 +74,26 @@ impl MetObjectApiRecord {
         None
     }
 
-    /// Try to download & cache the small image of the object if it's 2D artwork.
+    /// Try to download & cache the an image of the object if it's 2D artwork.
     ///
     /// If it's in the cache, returns the cached version. Otherwise, downloads and adds
     /// to cache.
     ///
     /// Returns (width, height, filename) on success. Dimensions are in physical meters, *not* pixels.
-    pub fn try_to_download_small_image(
+    pub fn try_to_download_image(
         &self,
         cache: &GalleryCache,
+        size: ImageSize,
     ) -> Result<Option<(f64, f64, String)>> {
         if let Some((width, height)) = self.overall_width_and_height() {
-            if self.primary_image_small.ends_with(".jpg") {
-                let small_image = format!("object-{}-small.jpg", self.object_id);
-                cache.cache_binary_url(&self.primary_image_small, &small_image)?;
-                return Ok(Some((width, height, small_image)));
+            let image_url = match size {
+                ImageSize::Small => &self.primary_image_small,
+                ImageSize::Large => &self.primary_image,
+            };
+            if image_url.ends_with(".jpg") {
+                let image_filename = format!("object-{}-{size}.jpg", self.object_id);
+                cache.cache_binary_url(&image_url, &image_filename)?;
+                return Ok(Some((width, height, image_filename)));
             }
         }
         Ok(None)
