c37871e983285ca0dee1d7d27fabec89af08a647
Atul Varma <varmaa@gmail.com>
2024-07-07 18:52:10 -0400

    Disable 'new gallery' button after user clicks it.

--- UserInterface/LayoutConfigContainer.gd
+++ UserInterface/LayoutConfigContainer.gd
@@ -25,10 +25,17 @@ func focus():
 func _on_do_layout_button_pressed():
 	var use_dense_layout := dense_layout_checkbox.button_pressed
 
+	# Disable this button while the layout is being generated, so the
+	# user can't click on it multiple times and cause chaos. This is also
+	# effectively a kind of low-fidelity loading indicator, letting the
+	# user know something is happening.
+	do_layout_button.disabled = true
+
 	# We need to un-pause the tree so we can poll for the response to our
 	# request to the Rust worker thread--otherwise the subsequent `await`
 	# will never return.
 	get_tree().paused = false
 
 	await MetObjects.layout(use_dense_layout)
+	do_layout_button.disabled = false
 	new_layout_complete.emit()
