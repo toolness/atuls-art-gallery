79b55d82fe95052c4045c162e5363b33f4ad53ce
Atul Varma <varmaa@gmail.com>
2024-07-09 05:52:25 -0400

    Populate gallery in order of player traversal through it.

--- Levels/moma_post_import.gd
+++ Levels/moma_post_import.gd
@@ -13,6 +13,13 @@ func _post_import(scene: Node) -> Node:
 				"height": wall.height
 			}
 			wall_data.push_back(datum)
+	wall_data.sort_custom(_sort_walls_by_name)
+
+	# We're going to reverse the order, because players actually navigate the gallery
+	# from back to front, so we want a sparsely-populated gallery to be populated at
+	# the earliest part of the player's journey.
+	wall_data.reverse()
+
 	var wall_data_json := JSON.stringify(wall_data, "  ")
 	var source_filename := get_source_file()
 	assert(source_filename.ends_with(".glb"), "Expected source filename to be GLB.")
@@ -22,3 +29,9 @@ func _post_import(scene: Node) -> Node:
 	file.close()
 	print("Wrote ", json_filename, ".")
 	return scene
+
+
+func _sort_walls_by_name(a: Dictionary, b: Dictionary) -> bool:
+	var a_name: String = a["name"]
+	var b_name: String = b["name"]
+	return a_name.naturalnocasecmp_to(b_name) < 0
