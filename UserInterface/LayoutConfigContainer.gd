e72a647451dec114af06037a5fbd6801b4052e79
Atul Varma <varmaa@gmail.com>
2024-07-08 21:44:19 -0400

    Show count while typing in filter field.

--- UserInterface/LayoutConfigContainer.gd
+++ UserInterface/LayoutConfigContainer.gd
@@ -8,18 +8,39 @@ signal new_layout_complete()
 
 @onready var dense_layout_checkbox: CheckBox = %DenseLayoutCheckBox
 @onready var filter_line_edit: LineEdit = %FilterLineEdit
+@onready var filter_results_label: Label = %FilterResultsLabel
 @onready var do_layout_button: Button = %DoLayoutButton
+@onready var back_button: Button = %BackButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	filter_line_edit.text_changed.connect(_on_filter_text_changed)
+
+
+func _on_filter_text_changed(filter: String):
+	if not filter:
+		filter_results_label.text = ""
+		return
+	var count := await MetObjects.count_met_objects(filter)
+	if filter_line_edit.text != filter:
+		# The player's input has changed, so our count is no
+		# longer accurate.
+		return
+	filter_results_label.text = str(count) + " artworks match your filter."
 
 
 func _on_back_button_pressed():
+	get_tree().paused = true
 	exit.emit()
 
 
 func focus():
+	# We need to un-pause the tree so we can poll for the response to our
+	# requests to the Rust worker thread--otherwise any `await`s we do
+	# will never return.
+	get_tree().paused = false
+
+	_on_filter_text_changed(filter_line_edit.text)
 	filter_line_edit.grab_focus()
 
 
@@ -32,15 +53,9 @@ func _on_do_layout_button_pressed():
 	# effectively a kind of low-fidelity loading indicator, letting the
 	# user know something is happening.
 	do_layout_button.disabled = true
-
-	# We need to un-pause the tree so we can poll for the response to our
-	# request to the Rust worker thread--otherwise the subsequent `await`
-	# will never return.
-	get_tree().paused = false
-
-	var count := await MetObjects.count_met_objects(filter)
-	print("Creating new gallery with ", count, " objects.")
+	back_button.disabled = true
 
 	await MetObjects.layout(filter, use_dense_layout)
 	do_layout_button.disabled = false
+	back_button.disabled = false
 	new_layout_complete.emit()
