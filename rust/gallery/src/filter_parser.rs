c9791069e92d65a7c55988f2404b9d09cf497f96
Atul Varma <varmaa@gmail.com>
2024-07-12 20:41:17 -0400

    Fitler multiple tokens.

--- rust/gallery/src/filter_parser.rs
+++ rust/gallery/src/filter_parser.rs
@@ -1,9 +1,5 @@
 use nom::{
-    branch::alt,
-    bytes::complete::{is_not, tag, take_until},
-    combinator::map,
-    sequence::delimited,
-    IResult,
+    branch::alt, bytes::complete::{is_not, tag, take_until}, character::complete::multispace1, combinator::map, multi::separated_list0, sequence::delimited, IResult
 };
 
 #[derive(Debug, Clone, PartialEq)]
@@ -37,12 +33,16 @@ pub fn filter_token(input: &str) -> IResult<&str, FilterToken> {
     alt((or, not, quoted_term, term))(input)
 }
 
+pub fn filter_tokens(input: &str) -> IResult<&str, Vec<FilterToken>> {
+    separated_list0(multispace1, filter_token)(input)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::filter_parser::{filter_token, FilterToken};
+    use crate::filter_parser::{filter_token, filter_tokens, FilterToken};
 
     #[test]
-    fn test_it_works() {
+    fn test_filter_token_works() {
         assert_eq!(filter_token("hi"), Ok(("", FilterToken::Term("hi"))));
         assert_eq!(filter_token("hi-there"), Ok(("", FilterToken::Term("hi-there"))));
         assert_eq!(filter_token("hi there"), Ok((" there", FilterToken::Term("hi"))));
@@ -50,5 +50,12 @@ mod tests {
         assert_eq!(filter_token("\"hi or - there\""), Ok(("", FilterToken::Term("hi or - there"))));
         assert_eq!(filter_token("-"), Ok(("", FilterToken::Not)));
         assert_eq!(filter_token("or"), Ok(("", FilterToken::Or)));
+    }
+
+    #[test]
+    fn test_filter_tokens_works() {
+        assert_eq!(filter_tokens("hi"), Ok(("", vec![FilterToken::Term("hi")])));
+        assert_eq!(filter_tokens("hi bub"), Ok(("", vec![FilterToken::Term("hi"), FilterToken::Term("bub")])));
+        assert_eq!(filter_tokens("hi \"bub sup\""), Ok(("", vec![FilterToken::Term("hi"), FilterToken::Term("bub sup")])));
     }
 }
